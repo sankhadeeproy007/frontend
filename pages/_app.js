0ce5430a2b72d050732b10ebbdd8252553fd2324
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2021-03-07 18:48:05 +0530

    chore: apollo client setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,30 @@
 /* eslint-disable */
-import Page from '../components/Page';
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 
+import Page from '../components/Page';
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-const App = ({ Component, pageProps }) => (
-  <div>
+const App = ({ Component, pageProps, apollo }) => (
+  <ApolloProvider client={apollo}>
     <Page>
       <Component {...pageProps} />
     </Page>
-  </div>
+  </ApolloProvider>
 );
 
-export default App;
+App.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+};
+
+export default withData(App);
