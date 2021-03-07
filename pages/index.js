d988ff4093903abefa932305a722c9b89c2f9a45
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2021-03-07 23:13:59 +0530

    feat: adds products fetching logic

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,1 @@
-import PropTypes from 'prop-types';
-
-const Home = ({ name }) => (
-  <>
-    <p>Hello {name} from home</p>
-    <h5>Life rocks</h5>
-  </>
-);
-
-Home.propTypes = {
-  name: PropTypes.string,
-};
-
-Home.defaultProps = {
-  name: 'Sankhadeep',
-};
-
-export default Home;
+export { default } from './products';
---
+++ pages/products.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Products from '../components/Products';
+
+const ProductPage = () => (
+  <div>
+    <Products />
+  </div>
+);
+export default ProductPage;
