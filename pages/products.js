499b91386a0d397c34cc210422269bce3700d1ba
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2021-03-28 13:27:36 +0530

    chore: adds pagination

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Pagination from '../components/Pagination';
 import Products from '../components/Products';
 
 const ProductPage = () => (
   <div>
+    <Pagination page={1} />
     <Products />
+    <Pagination page={1} />
   </div>
 );
 export default ProductPage;
